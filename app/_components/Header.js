95abdf86651a36b1021e239eaa26d57b217e65ff
Adrian Ortega <adrianog.sw@gmail.com>
2024-12-22 11:56:01 -0600

    Add  language display to header.
    Changed some of the tailwind css properties in
    the main div container in an attempt to reduce
    the amount of properties written.
    
    The language div makes use of many tailwind css
    properties but they are mostly for text formatting
    and not for flex display.

--- app/_components/Header.js
+++ app/_components/Header.js
@@ -1,22 +1,33 @@
-import Image from 'next/image';
+import Image from "next/image";
 import { useTranslations } from "@/app/hooks/use-translations";
 
 export const Header = () => {
-    const { t } = useTranslations();
-    const title = t('shared.title');
-    return (
-        <header
-            className='bg-black flex justify-start items-center flex-grow h-[70px]'
-        >
-            <div className='w-[80px] flex justify-center'>
-                <Image
-                    src="/brand/logo.png"
-                    width="40" height="40"
-                    alt={title}
-                    title={title}
-                />
-            </div>
-            <h1 className='text-white font-sans text-3xl'>{title}</h1>
-        </header>
-    )
-}
+  const { t } = useTranslations();
+  const title = t("shared.title");
+  return (
+    <header className="bg-black flex justify-between items-center h-[70px]">
+      <div className="w-[80px]">
+        <Image
+          className="justify-self-center"
+          src="/brand/logo.png"
+          width="40"
+          height="40"
+          alt={title}
+          title={title}
+        />
+      </div>
+      <h1 className="text-white font-sans text-3xl flex-grow">{title}</h1>
+      <div className="flex gap-4 py-2 px-4 mx-4 bg-gray-500 rounded-lg">
+        <div className="text-white font-bold">ES | MX</div>
+        <Image
+          className="border-2 border-gray-800"
+          src="/langs/mexico.png"
+          width="30"
+          height="30"
+          alt="mexican flag"
+          title="Mexican flag"
+        ></Image>
+      </div>
+    </header>
+  );
+};
