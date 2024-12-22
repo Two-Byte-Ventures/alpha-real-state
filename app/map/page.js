f5717d546f84fe2d40fb64947cb4562db0600f57
Sergio Guerra <sergioguerra1991@gmail.com>
2024-12-22 10:14:35 -0600

    Add Map Placeholder

--- app/map/page.js
+++ app/map/page.js
@@ -8,22 +8,26 @@ export default function Page() {
   const mapContainerRef = useRef();
 
   useEffect(() => {
-    mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN;
-    mapRef.current = new mapboxgl.Map({
-      container: mapContainerRef.current,
-      center: [-103.668, 23.420],
-      zoom: 4,
-      style: process.env.NEXT_PUBLIC_MAPBOX_STYLE
-    });
+    if (mapboxgl.supported() && process.env.NEXT_PUBLIC_MAPBOX_ENABLE == 'true') {
+      mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN;
+      mapRef.current = new mapboxgl.Map({
+        container: mapContainerRef.current,
+        center: [-103.668, 23.420],
+        zoom: 4,
+        style: process.env.NEXT_PUBLIC_MAPBOX_STYLE
+      });
 
-    return () => {
-      mapRef.current.remove()
+      return () => {
+        mapRef.current.remove()
+      }
     }
   }, [])
 
   return (
     <div className="w-full h-screen flex justify-center items-center">
-      <div id='map-container' className="w-full h-full" ref={mapContainerRef}/>
+      <div id='map-container' className="w-full h-full flex justify-center items-center" ref={mapContainerRef}>
+        MAP
+      </div>
     </div>
   );
 }
