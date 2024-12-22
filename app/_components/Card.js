f9fee263f94aa83f1aa227fdf34aabdcd3fea400
Adrian Ortega <adrianog.sw@gmail.com>
2024-12-22 14:20:09 -0600

    Add image backgroud to Card type

--- app/_components/Card.js
+++ app/_components/Card.js
@@ -5,15 +5,25 @@ export const Card = ({ type }) => {
   const { t } = useTranslations();
 
   return (
-    <div className="flex justify-center items-center shadow-lg border w-[250px] h-[150px] rounded-lg">
+    <div className="relative flex flex-col justify-end  shadow-lg border w-[250px] h-[150px] rounded-lg background-white">
       <Image
-        src={`/types/${type}.jpg`}
-        width="25"
-        height="25"
+        className="rounded-lg z-0 "
+        src={`/types/${type}_bg.jpg`}
+        objectFit="cover"
+        fill={true}
         alt={t(`types.${type}`)}
-        title={t(`types.${type}`)}
       />
-      {t(`types.${type}`)}
+
+      <div className="bg-white py-2 flex justify-center z-10 rounded-b-lg">
+        <Image
+          src={`/types/${type}.jpg`}
+          width="25"
+          height="25"
+          alt={t(`types.${type}`)}
+          title={t(`types.${type}`)}
+        />
+        {t(`types.${type}`)}
+      </div>
     </div>
   );
 };
