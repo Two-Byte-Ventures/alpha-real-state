e366041abcf401c5794a6935599d7fe0744b05a5
Sergio Guerra <sergioguerra1991@gmail.com>
2024-12-22 02:25:18 -0600

    Add Mapbox

--- app/map/page.js
+++ app/map/page.js
@@ -1,7 +1,29 @@
+"use client"
+import { useEffect, useRef } from "react";
+import mapboxgl from "mapbox-gl";
+import 'mapbox-gl/dist/mapbox-gl.css';
+
 export default function Page() {
+  const mapRef = useRef();
+  const mapContainerRef = useRef();
+
+  useEffect(() => {
+    mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN;
+    mapRef.current = new mapboxgl.Map({
+      container: mapContainerRef.current,
+      center: [-103.668, 23.420],
+      zoom: 4,
+      style: process.env.NEXT_PUBLIC_MAPBOX_STYLE
+    });
+
+    return () => {
+      mapRef.current.remove()
+    }
+  }, [])
+
   return (
     <div className="w-full h-screen flex justify-center items-center">
-      <h1>Map</h1>
+      <div id='map-container' className="w-full h-full" ref={mapContainerRef}/>
     </div>
   );
 }
