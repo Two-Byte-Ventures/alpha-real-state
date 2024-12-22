8c678adefea4bf971f66f30a6f28ce0b2f08888c
Adrian Ortega <adrianog.sw@gmail.com>
2024-12-22 13:33:19 -0600

    Refactored Card component

--- app/_components/Card.js
+++ app/_components/Card.js
@@ -1,7 +1,19 @@
-export const Card = ({ children, className }) => {
-    return (
-        <div className={`${className} shadow-lg border w-[250px] h-[150px] rounded-lg`}>
-            {children}
-        </div>
-    );
+import Image from "next/image";
+import { useTranslations } from "../hooks/use-translations";
+
+export const Card = ({ type }) => {
+  const { t } = useTranslations();
+
+  return (
+    <div className="flex justify-center items-center shadow-lg border w-[250px] h-[150px] rounded-lg">
+      <Image
+        src={`/types/${type}.jpg`}
+        width="25"
+        height="25"
+        alt={t(`types.${type}`)}
+        title={t(`types.${type}`)}
+      />
+      {t(`types.${type}`)}
+    </div>
+  );
 };
--- app/page.js
+++ app/page.js
@@ -1,10 +1,11 @@
-import Image from "next/image";
 import Link from "next/link";
 import { Card } from "@/app/_components/Card";
 import { useTranslations } from "@/app/hooks/use-translations";
 
 export default function Page() {
   const { t } = useTranslations();
+  const cardTypes = ["industrial", "commercial", "residential"];
+
   return (
     <div className="grid grid-rows-[auto_auto_1fr_1fr] grid-cols-3 justify-items-center items-center h-full w-full">
       <h1 className="text-4xl my-24 col-span-full">
@@ -17,37 +18,11 @@ export default function Page() {
       </p>
 
       <section className="col-span-full flex self-center space-x-36">
-        <Card className="flex justify-center items-center">
-          <Image
-            src="/types/industrial.jpg"
-            width="25"
-            height="25"
-            alt={t("types.industrial")}
-            title={t("types.industrial")}
-          />
-          {t("types.industrial")}
-        </Card>
-        <Card className="flex justify-center items-center">
-          <Image
-            src="/types/comercial.png"
-            width="25"
-            height="25"
-            alt={t("types.commercial")}
-            title={t("types.commercial")}
-          />
-          {t("types.commercial")}
-        </Card>
-        <Card className="flex justify-center items-center">
-          <Image
-            src="/types/residential.jpg"
-            width="25"
-            height="25"
-            alt={t("types.residential")}
-            title={t("types.residential")}
-          />
-          {t("types.residential")}
-        </Card>
+        {cardTypes.map((type) => (
+          <Card type={type} key={type} />
+        ))}
       </section>
+
       <Link className="col-span-full self-start my-12" href="/map">
         <button className="bg-black text-white w-[200px] h-[45px] rounded-xl text-lg hover:bg-zinc-700 hover:shadow-2xl">
           {t("shared.start")}
