6d1448acdb8b17dfec13ebe7ea67214ed0f102b4
Sergio Guerra <sergioguerra1991@gmail.com>
2024-12-22 23:41:17 -0600

    Map uses custom style

--- app/map/page.js
+++ app/map/page.js
@@ -7,26 +7,28 @@ export default function Page() {
   const mapRef = useRef();
   const mapContainerRef = useRef();
 
-  useEffect(() => {
-    if (mapboxgl.supported() && process.env.NEXT_PUBLIC_MAPBOX_ENABLE == 'true') {
-      mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN;
-      mapRef.current = new mapboxgl.Map({
-        container: mapContainerRef.current,
-        center: [-103.668, 23.420],
-        zoom: 4,
-        style: process.env.NEXT_PUBLIC_MAPBOX_STYLE
-      });
+  const isMapboxSupported = mapboxgl.supported() && process.env.NEXT_PUBLIC_MAPBOX_ENABLE == 'true';
 
-      return () => {
-        mapRef.current.remove()
-      }
+  useEffect(() => {
+    if (!isMapboxSupported) return;
+
+    mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN;
+    mapRef.current = new mapboxgl.Map({
+      container: mapContainerRef.current,
+      center: [-100.668, 23.420],
+      zoom: 5,
+      style: process.env.NEXT_PUBLIC_MAPBOX_GL_STYLE_URL
+    });
+
+    return () => {
+      mapRef.current.remove();
     }
   }, [])
 
   return (
     <div className="w-full h-screen flex justify-center items-center">
-      <div id='map-container' className="w-full h-full flex justify-center items-center" ref={mapContainerRef}>
-        MAP
+      <div id='map-container' className="w-full h-full" ref={mapContainerRef}>
+        <div className="w-full h-full flex justify-center items-center">MAP</div>
       </div>
     </div>
   );
