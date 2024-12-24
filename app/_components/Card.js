e2e18451bedfca4f7f864c54acd3f19f95686dbd
Adrian Ortega <adrianog.sw@gmail.com>
2024-12-24 10:28:47 -0600

    Use Tailwind and CSS to properly center Card background images.

--- app/_components/Card.js
+++ app/_components/Card.js
@@ -5,14 +5,20 @@ export const Card = ({ type }) => {
   const { t } = useTranslations();
 
   return (
-    <div className="relative flex flex-col justify-end  shadow-lg border w-[250px] h-[150px] rounded-lg background-white shadow-violet-100">
-      <Image
-        className="rounded-lg z-0 "
-        src={`/types/${type}_bg.jpg`}
-        objectFit="cover"
-        fill={true}
-        alt={t(`types.${type}`)}
-      />
+    <div className="flex flex-col shadow-lg border w-[250px] h-[150px] rounded-lg background-white shadow-violet-100">
+      {/* The reason why we use style is because tailwind does not process dynamic
+      paths for image URLs, thus the image won´t ever be displayed.
+      
+      The way to "fix" this is by using the CSS style property directly rather
+      than Tailwind.
+
+      For some reason Next.js was not properly centering the images on the space
+      allocated to it. Thus we use this trick with Tailwind and CSS.
+      */}
+      <div
+        className={`flex-grow bg-cover bg-center rounded-t-lg`}
+        style={{ backgroundImage: `url('/types/${type}_bg.jpg')` }}
+      ></div>
 
       <div className="bg-white py-2 flex justify-center z-10 rounded-b-lg">
         <Image
