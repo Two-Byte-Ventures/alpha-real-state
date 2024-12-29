98ce7b82b2347e2d453a0e068b3d9a900f3fcc14
Adrian Ortega <adrianog.sw@gmail.com>
2024-12-29 15:59:22 -0600

    Add HousingOverview component

---
+++ app/_components/HousingOverview.js
@@ -0,0 +1,48 @@
+import { Card, CardBody, CardHeader } from "@nextui-org/react";
+import OverviewRow from "./OverviewRow";
+
+function HousingOverview() {
+  const dwellings = [
+    {
+      id: 1,
+      price: "4,513,900",
+      type: "house",
+      percentage: "11.9%",
+      increased: true,
+    },
+    {
+      id: 2,
+      price: "2,000,760",
+      type: "plaza",
+      percentage: "15.9%",
+      increased: true,
+    },
+    {
+      id: 3,
+      price: "10,913,900",
+      type: "industrial",
+      percentage: "9.9%",
+      increased: false,
+    },
+  ];
+  return (
+    <Card isBlurred className="border-none" shadow="none">
+      <CardHeader className="text-small font-bold justify-center">
+        <h1 className="text-xl text-violet-700 font-serif">Querétaro</h1>
+      </CardHeader>
+      <CardBody className="text-tiny">
+        {dwellings.map((dwelling) => (
+          <OverviewRow
+            key={dwelling.id}
+            price={dwelling.price}
+            type={dwelling.type}
+            percentage={dwelling.percentage}
+            increased={dwelling.increased}
+          />
+        ))}
+      </CardBody>
+    </Card>
+  );
+}
+
+export default HousingOverview;
--- app/map/page.js
+++ app/map/page.js
@@ -14,6 +14,7 @@ import "mapbox-gl/dist/mapbox-gl.css";
 import CallToAction from "../_components/CallToAction";
 import HousePrice from "../_components/HousePrice";
 import HouseDetails from "../_components/HouseDetails";
+import HousingOverview from "../_components/HousingOverview";
 
 export default function Page() {
   const mapRef = useRef();
@@ -47,12 +48,7 @@ export default function Page() {
             <Button className="m-2">State/Province Placeholder</Button>
           </PopoverTrigger>
           <PopoverContent>
-            <Card isBlurred className="border-none" shadow="none">
-              <CardHeader className="text-small font-bold">
-                State/Province
-              </CardHeader>
-              <CardBody className="text-tiny">Popover content</CardBody>
-            </Card>
+            <HousingOverview />
           </PopoverContent>
         </Popover>
 
