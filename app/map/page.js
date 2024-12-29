f7b91f7abc92a2a49da4d82ad1a798d6326078a2
Adrian Ortega <adrianog.sw@gmail.com>
2024-12-29 13:30:58 -0600

    Refactored the HousePrice inside the CardHeader

--- app/map/page.js
+++ app/map/page.js
@@ -15,6 +15,7 @@ import { Card, CardHeader, CardBody, CardFooter } from "@nextui-org/card";
 import mapboxgl from "mapbox-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
 import CallToAction from "../_components/CallToAction";
+import HousePrice from "../_components/HousePrice";
 
 export default function Page() {
   const mapRef = useRef();
@@ -62,28 +63,7 @@ export default function Page() {
           <PopoverContent>
             <Card isBlurred className="border-none" shadow="none">
               <CardHeader className="text-small font-bold">
-                <div className="flex font-sans justify-between items-center flex-grow">
-                  <h1 className="text-3xl font-serif justify-self-start">
-                    $4,000,000
-                  </h1>
-                  <div className="ml-4 flex items-center justify-self-end gap-x-1">
-                    <p>MXN</p>
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      strokeWidth="1.5"
-                      stroke="currentColor"
-                      class="size-6"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        stroke-linejoin="round"
-                        d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9 5.25h.008v.008H12v-.008Z"
-                      />
-                    </svg>
-                  </div>
-                </div>
+                <HousePrice price="4,000,000" />
               </CardHeader>
               <Divider />
               <CardBody className="text-tiny">
