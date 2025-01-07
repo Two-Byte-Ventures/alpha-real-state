9796cf376d3774112fe83049eb2cb4afbc14d0c1
Sergio Guerra <sergioguerra1991@gmail.com>
2025-01-07 00:55:12 -0600

    Use react-map-gl and Add MX geojsons

--- app/map/page.js
+++ app/map/page.js
@@ -1,5 +1,5 @@
 "use client";
-import { useEffect, useRef } from "react";
+import { useRef, useMemo, useState, useCallback } from "react";
 import {
   Popover,
   PopoverTrigger,
@@ -9,77 +9,122 @@ import {
 } from "@nextui-org/react";
 
 import { Card, CardHeader, CardBody, CardFooter } from "@nextui-org/card";
-import mapboxgl from "mapbox-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
 import CallToAction from "../_components/CallToAction";
 import HousePrice from "../_components/HousePrice";
 import HouseDetails from "../_components/HouseDetails";
 import HousingOverview from "../_components/HousingOverview";
+import Map, { Source, Layer } from 'react-map-gl';
+import states from '@/geojson/states.json';
+import municipalities from '@/geojson/municipalities.json';
 
 export default function Page() {
   const mapRef = useRef();
-  const mapContainerRef = useRef();
+  const [isMapLoaded, setIsMapLoaded] = useState(false);
 
-  const isMapboxSupported =
-    mapboxgl.supported() && process.env.NEXT_PUBLIC_MAPBOX_ENABLE == "true";
+  const mapboxToken = useMemo(()=>{
+    if (process.env.NEXT_PUBLIC_MAPBOX_ENABLE == "true"){
+      return process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN;
+    }
+    return '';
+  }, []);
 
-  useEffect(() => {
-    if (!isMapboxSupported) return;
-
-    mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN;
-    mapRef.current = new mapboxgl.Map({
-      container: mapContainerRef.current,
-      center: [-100.668, 23.42],
-      zoom: 5,
-      style: process.env.NEXT_PUBLIC_MAPBOX_GL_STYLE_URL,
-    });
-
-    return () => {
-      mapRef.current.remove();
-    };
-  }, [isMapboxSupported]);
+  const onLoadHandler = useCallback(() => {
+    setIsMapLoaded(true);
+  }, []);
 
   return (
-    <div className="w-full h-screen flex justify-center items-center">
-      <div id="map-container" className="w-full h-full" ref={mapContainerRef}>
-        {/*  */}
-        <Popover placement="bottom">
-          <PopoverTrigger>
-            <Button className="m-2">State/Province Placeholder</Button>
-          </PopoverTrigger>
-          <PopoverContent>
-            <HousingOverview />
-          </PopoverContent>
-        </Popover>
+    <Map
+      ref={mapRef}
+      className="w-full h-screen flex justify-center items-center"
+      mapboxAccessToken={mapboxToken}
+      initialViewState={{
+        longitude: -100.668,
+        latitude: 23.42,
+        zoom: 4.8
+      }}
+      mapStyle={process.env.NEXT_PUBLIC_MAPBOX_GL_STYLE_URL}
+      onLoad={onLoadHandler}
+    >
+      {isMapLoaded && (
+          [
+            <Source key="statesSource" type="geojson" data={states}>
+              <Layer
+                id="states"
+                type="fill"
+                paint={{
+                  "fill-color": "#3bac35",
+                  "fill-opacity": 0.5,
+                }}
+                filter={["==", "state_name", "Querétaro"]}
+              />
+              <Layer
+                id="states-line"
+                type="line"
+                paint={{
+                  "line-color": "#277916",
+                  "line-width": 1,
+                }}
+                filter={["==", "state_name", "Querétaro"]}
+                />
+            </Source>
+          ,
+            <Source key="municipalitiesSource" type="geojson" data={municipalities}>
+              <Layer
+                id="municipalities"
+                type="fill"
+                paint={{
+                  "fill-color": "#3bac35",
+                  "fill-opacity": 0.5,
+                }}
+                filter={["==", "mun_name", "Querétaro"]}
+              />
+              <Layer
+                id="municipalities-line"
+                type="line"
+                paint={{
+                  "line-color": "#277916",
+                  "line-width": 1,
+                }}
+                filter={["==", "mun_name", "Querétaro"]}
+                />
+            </Source>
+          ]
+      )}
+      <Popover placement="bottom">
+        <PopoverTrigger>
+          <Button className="m-2">State/Province Placeholder</Button>
+        </PopoverTrigger>
+        <PopoverContent>
+          <HousingOverview />
+        </PopoverContent>
+      </Popover>
 
-        <Popover placement="bottom">
-          <PopoverTrigger>
-            <Button className="m-2">House Placeholder</Button>
-          </PopoverTrigger>
+      <Popover placement="bottom">
+        <PopoverTrigger>
+          <Button className="m-2">House Placeholder</Button>
+        </PopoverTrigger>
 
-          <PopoverContent>
-            <Card isBlurred className="border-none" shadow="none">
-              {/*  */}
-              <CardHeader className="text-small font-bold">
-                <HousePrice price="4,000,000" />
-              </CardHeader>
+        <PopoverContent>
+          <Card isBlurred className="border-none" shadow="none">
+            <CardHeader className="text-small font-bold">
+              <HousePrice price="4,000,000" />
+            </CardHeader>
 
-              <Divider />
+            <Divider />
 
-              <CardBody className="text-tiny">
-                <HouseDetails />
-              </CardBody>
+            <CardBody className="text-tiny">
+              <HouseDetails />
+            </CardBody>
 
-              <Divider />
+            <Divider />
 
-              <CardFooter className="text-small">
-                <CallToAction />
-              </CardFooter>
-              {/*  */}
-            </Card>
-          </PopoverContent>
-        </Popover>
-      </div>
-    </div>
+            <CardFooter className="text-small">
+              <CallToAction />
+            </CardFooter>
+          </Card>
+        </PopoverContent>
+      </Popover>
+    </Map>
   );
 }
