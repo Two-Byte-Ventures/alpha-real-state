b8f38233d0a5e6c6741ffd7f27ea7afdc6f95caa
Sergio Guerra <sergioguerra1991@gmail.com>
2025-06-17 22:35:51 -0600

    Add initial permissions migration for public read access to housing and related entities

---
+++ migrations/01_initial_permissions.sql
@@ -0,0 +1,29 @@
+-- Allow public read access to housing
+CREATE POLICY "Public can read housing"
+ON public.housing
+FOR SELECT TO anon
+USING (true);
+
+-- Allow read access to all countries
+CREATE POLICY "Allow read access to all countries"
+ON countries
+FOR SELECT
+USING (true);
+
+-- Allow read access to all states
+CREATE POLICY "Allow read access to all states"
+ON states
+FOR SELECT
+USING (true);
+
+-- Allow read access to all municipalities
+CREATE POLICY "Allow read access to all municipalities"
+ON municipalities
+FOR SELECT
+USING (true);
+
+-- Allow read access to all housing_types
+CREATE POLICY "Allow read access to all housing_types"
+ON housing_types
+FOR SELECT
+USING (true);
